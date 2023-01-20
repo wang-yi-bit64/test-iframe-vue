996f60395aab5e835975c68f07b6d471f725b1da
wangyi <2851813754@qq.com>
2023-01-20 11:03:08 +0800

    错误的实践

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -11,24 +11,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
--- src/components/TestIframe.vue
+++ src/components/TestIframe.vue
@@ -1,57 +1,47 @@
 <script setup lang="ts">
-import { ref, watch, onMounted, onUnmounted, watchEffect } from "vue";
+import { ref, watch, onMounted, onUnmounted, watchEffect, nextTick } from "vue";
 import pdfFile from "../assets/326_ParcelLabelingGuide-1 - 副本【搜狗文档翻译_双语_英译中】.pdf";
+const pdfUrl = ref(`${window.location.origin}${pdfFile}`);
+
 const iframeRef = ref();
 onMounted(() => {
-  console.log("挂在页面");
+  console.log("挂在页面", window.location);
 });
 onUnmounted(() => {
   console.log("卸载");
 });
-// watch(
-//   iframeRef,
-//   (newValue, oldValue) => {
-//     console.log(newValue, oldValue, "watchers");
-//     console.log(newValue.contentWindow);
-//     if (newValue) {
-//       console.log(newValue.contentWindow, "ele");
-//       newValue.onload = () => {
-//         console.log("load watchers");
-//         console.log(newValue.contentWindow);
-//         newValue.contentWindow.addEventListener("afterprint", () => {
-//           console.log("after print addEventListener");
-//         });
-//         let mediaQueryList = newValue.contentWindow.matchMedia("print");
-//         mediaQueryList.addListener((mql) => {
-//           if (mql.matches) {
-//             console.log("onbeforeprint equivalent");
-//           } else {
-//             console.log("onafterprint equivalent");
-//           }
-//         });
-//         newValue.contentWindow.onafterprint = afterprintfn;
-//       };
-//     }
-//   },
-//   { deep: true }
-// );
-watchEffect(() => {
-  if (iframeRef.value) {
-    iframeRef.value.onload = () => {
-      console.log("load watchEffect");
-      iframeRef.value.contentWindow.addEventListener("afterprint", () => {
-        console.log("after print addEventListener watchEffect");
-      });
-      iframeRef.value.onafterprint = afterprintfn;
-    };
-  }
-});
+watch(
+  iframeRef,
+  (newValue, oldValue) => {
+    console.log(newValue, oldValue, "watchers");
+    console.log(newValue.contentWindow);
+    if (newValue) {
+      console.log(newValue.contentWindow, "ele");
+      newValue.onload = () => {
+        console.log("load watchers");
+        console.log(newValue.contentWindow);
+        newValue.contentWindow.addEventListener("afterprint", function () {
+          console.log("after print addEventListener");
+        });
+        let mediaQueryList = newValue.contentWindow.matchMedia("print");
+        mediaQueryList.addListener(function (mql) {
+          if (mql.matches) {
+            console.log("onbeforeprint equivalent");
+          } else {
+            console.log("onafterprint equivalent");
+          }
+        });
+        // newValue.contentWindow.onafterprint = afterprintfn;
+      };
+    }
+  },
+  { deep: true }
+);
 
-const afterprintfn = () => {
+const onaAfterprintfn = () => {
   console.log("after print afterprintfn");
 };
 
-const pdfUrl = ref(pdfFile);
 const handlePdfClick = () => {
   console.log("iframeRef", iframeRef.value, iframeRef.value.contentWindow);
   // iframeRef.value.contentWindow.addEventListener('afterprint', () => {
@@ -61,29 +51,34 @@ const handlePdfClick = () => {
   //   console.log('after print')
   // })
 
-  iframeRef.value.contentWindow.onload = onload;
+  // iframeRef.value.contentWindow.onload = onload;
 
   iframeRef.value.contentWindow.print();
-  iframeRef.value.contentWindow.addEventListener("afterprint", () => {
-    console.log("after print: clicked");
-  });
+  // iframeRef.value.contentWindow.addEventListener("afterprint", () => {
+  //   console.log("after print: clicked");
+  // });
 };
-const onload = () => {
-  console.log("load", iframeRef.value.contentWindow);
+async function loaded() {
+  let IframeEle = document.getElementById("Iframe");
 
-  iframeRef.value.contentWindow.addEventListener("afterprint", () => {
+  console.log("loaded", IframeEle);
+  console.log("IframeEle", IframeEle.contentWindow);
+  await nextTick();
+  IframeEle.contentWindow.addEventListener("afterprint", () => {
     console.log("after print");
   });
 
-  // let mediaQueryList = iframeRef.value.contentWindow.matchMedia('print')
-  // mediaQueryList.addListener((mql) => {
-  //   if (mql.matches) {
-  //     console.log('onbeforeprint equivalent');
-  //   } else {
-  //     console.log('onafterprint equivalent');
-  //   }
-  // });
-};
+  let mediaQueryList = IframeEle.contentWindow.matchMedia("print");
+  console.log("mediaQueryList", mediaQueryList);
+  mediaQueryList.addEventListener("change", function (mql) {
+    console.log("mql", mql);
+    if (mql.matches) {
+      console.log("onbeforeprint equivalent");
+    } else {
+      console.log("onafterprint equivalent");
+    }
+  });
+}
 </script>
 
 <template>
@@ -94,8 +89,10 @@ const onload = () => {
       id="Iframe"
       :src="pdfUrl"
       ref="iframeRef"
-      @onload="onload"
-    />
+      @load="loaded"
+      @afterprint="onaAfterprintfn"
+    >
+    </iframe>
     <button @click="handlePdfClick">打印</button>
   </div>
 </template>
